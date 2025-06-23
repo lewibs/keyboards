2fd81707d0c6de70de9fedb883a8204c41f28db1
Your Name <you@example.com>
2025-06-23 12:46:11 -0400

    changed spinner to scroll moved udo redo to a layer

--- blank/keymap.c
+++ blank/keymap.c
@@ -30,11 +30,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,                    KC_BSPC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOTE,                  KC_TAB,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                    KC_ENTER, KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLASH,
-        KC_LCTL, KC_LALT, MO(3),   KC_LSFT, MO(2),   XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,   MO(1),   MO(4),   XXXXXXX, KC_LGUI, KC_RALT
+        KC_LCTL, KC_LALT, MO(3),   KC_LSFT, MO(2),   XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,   MO(1),   MO(4),   KC_LGUI, XXXXXXX, KC_RALT
         ),
 
     //Numbers
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
 
-    //mouse TODO
+    //mouse
     [4] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, KC_RCLC, KC_MUP,  KC_LCLC, XXXXXXX, KC_TRNS,                   KC_TRNS, XXXXXXX, KC_LCLC, KC_UP,   KC_RCLC, XXXXXXX,
@@ -90,13 +90,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   {
             //Encoder 1
-            ENCODER_CCW_CW(KC_UNDO, KC_REDO),
+            ENCODER_CCW_CW(KC_PDWN, KC_PUP),
             //Encoder 2
             ENCODER_CCW_CW(XXXXXXX, XXXXXXX),
             },
     [1] =   {
             //Encoder 1
-            ENCODER_CCW_CW(KC_TRNS, KC_TRNS),
+            ENCODER_CCW_CW(KC_UNDO, KC_REDO),
             //Encoder 2
             ENCODER_CCW_CW(KC_TRNS, KC_TRNS),
             },
